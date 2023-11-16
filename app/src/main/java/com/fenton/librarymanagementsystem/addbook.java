6249da5287dc73976b664e559942e77213c831cb
Sumit <sumitsaha395@gmail.com>
2023-11-17 00:11:50 +0530

    This is my third commit.

--- app/src/main/java/com/fenton/librarymanagementsystem/addbook.java
+++ app/src/main/java/com/fenton/librarymanagementsystem/addbook.java
@@ -1,14 +1,61 @@
 package com.fenton.librarymanagementsystem;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class addbook extends AppCompatActivity {
+
+    private static final int PICK_FILE_REQUEST = 1;
+    Button addbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addbook);
+
+        Button chooseFileButton = findViewById(R.id.openfilechooser);
+        addbutton=findViewById(R.id.addbutton);
+
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        chooseFileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");  // You can specify the type of files you want to allow here, e.g., "image/*" for images
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), PICK_FILE_REQUEST,null);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Handle errors
+            Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_FILE_REQUEST && resultCode == RESULT_OK && data != null) {
+
+//            TODO: Handle the selected file
+        }
     }
 }
